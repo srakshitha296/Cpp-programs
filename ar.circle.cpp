c7b107e2c727023ddd18a5101ae8caa6bdeaa848
Rakshitha Shetty <111368698+srakshitha296@users.noreply.github.com>
2022-10-31 20:57:17 +0530

    Add files via upload

---
+++ ar.circle.cpp
@@ -0,0 +1,14 @@
+//Program to find Area and Circumference of a circle given the radius
+
+#include<iostream>
+using namespace std;
+#define pi 3.142
+// pi is symbolic constant
+int main()
+{
+   float r;
+   cout<<"Enter the radius of the circle :";
+   cin>>r;
+   cout<<"Area :"<<pi*r*r<<"\nCircumference :"<<2*pi*r;
+  return 0;
+}
